41b403cbe0830635e9153300f87d27eb32bbe7a7
Islom Pardaboyev <ipardaboyev574@gmail.com>
2024-10-03 10:33:58 +0500

    init

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,33 +1,80 @@
-import axios from 'axios'
-import { Component } from 'react'
-import { ProductsData } from './Home'
+import axios from "axios";
+import { Component } from "react";
+import { categoriesColor, ProductsData } from "./Home";
+import { FaStar } from "react-icons/fa6";
 
 interface Props {
-  data: ProductsData
+  data: ProductsData;
+  isLoading: boolean;
 }
-const productId = JSON.parse(window.localStorage.getItem('productId') as string)
-export class About extends Component <{}, Props> {
+const productId = JSON.parse(
+  window.localStorage.getItem("productId") as string
+);
+export class About extends Component<{}, Props> {
   constructor(props: any) {
     super(props);
     this.state = {
-      data: {} as ProductsData
+      data: {} as ProductsData,
+      isLoading: true,
     };
-    
   }
   componentDidMount(): void {
     axios.get(`https://fakestoreapi.com/products/${productId}`).then((res) => {
       this.setState({ data: res.data });
+      this.setState({isLoading: !this.state.isLoading})
     });
   }
   render() {
-    
-    
+    console.log(this.state.data);
+
+    if(this.state.isLoading){
+      return (
+        <section className="flex items-center justify-center h-screen">loading...</section>
+      )
+    }
+
     return (
-      <section className='w-screen h-screen'>
-        <img src={this.state.data.image} alt="" />
+      <section className="w-screen h-screen flex items-center justify-center">
+        <div className="container mx-auto">
+          <div className="grid grid-cols-2 ">
+            <div className="col-span-1">
+              <img
+                className="h-[300px] mx-auto"
+                src={this.state.data.image}
+                alt=""
+              />
+            </div>
+            <div className="col-span-1">
+              <h1 className="font-medium text-neutral-400">
+                {this.state.data.title}
+              </h1>
+              <h1 className="font-medium  text-3xl">
+                ${this.state.data.price}
+              </h1>
+              <h1 className={`${categoriesColor[this.state.data.category]}`}>
+                {this.state.data.category}
+              </h1>
+              <h1 className="font-medium capitalize mt-5 text-neutral-600">
+                {this.state.data.description}
+              </h1>
+              <div className="flex items-center mt-5 gap-16">
+                <p className="flex items-center gap-3 font-bold text-neutral-400">
+                  Rate:{" "}
+                  <span className="flex items-center gap-2">
+                    {this.state.data.rating?.rate}{" "}
+                    <FaStar className="text-yellow-500" />
+                  </span>
+                </p>
+                <p className="flex items-center gap-3 font-bold text-neutral-400">
+                  Count: <span>{this.state.data.rating?.count}</span>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
       </section>
-    )
+    );
   }
 }
 
-export default About
+export default About;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,24 @@
 import axios from "axios";
 import { Component } from "react";
+import { CgEye } from "react-icons/cg";
 import { FaStar } from "react-icons/fa6";
+export const categoriesColor: { [key: string]: string } = {
+  "men's clothing":
+    "bg-blue-500 text-white inline-block px-2.5 py-0.5 rounded-full text-xs font-medium",
+  "women's clothing":
+    "bg-red-500 text-white inline-block px-2.5 py-0.5 rounded-full text-xs font-medium",
+  kids: "bg-green-500 text-white inline-block px-2.5 py-0.5 rounded-full text-xs font-medium",
+  electronics:
+    "bg-yellow-500 text-white inline-block px-2.5 py-0.5 rounded-full text-xs font-medium",
+  jewelery:
+    "bg-purple-500 text-white inline-block px-2.5 py-0.5 rounded-full text-xs font-medium",
+};
 
 export interface ProductsData {
   id: number;
   title: string;
   price: number;
-  desc: string;
+  description: string;
   category: string;
   image: string;
   rating: RatingData;
@@ -41,19 +53,6 @@ export class Home extends Component<{}, HomeState> {
   }
 
   render() {
-
-    const categoriesColor: { [key: string]: string } = {
-      "men's clothing":
-        "bg-blue-500 text-white inline-block px-2.5 py-0.5 rounded-full text-xs font-medium",
-      "women's clothing":
-        "bg-red-500 text-white inline-block px-2.5 py-0.5 rounded-full text-xs font-medium",
-      kids: "bg-green-500 text-white inline-block px-2.5 py-0.5 rounded-full text-xs font-medium",
-      electronics:
-        "bg-yellow-500 text-white inline-block px-2.5 py-0.5 rounded-full text-xs font-medium",
-      jewelery:
-        "bg-purple-500 text-white inline-block px-2.5 py-0.5 rounded-full text-xs font-medium",
-    };
-
     if (this.state.isLoading) {
       return (
         <section className="flex items-center justify-center h-screen">
@@ -64,44 +63,77 @@ export class Home extends Component<{}, HomeState> {
     return (
       <section className="py-10">
         <div className="container mx-auto">
-        <div className="my-2 flex items-center justify-between">
-        <h1 className="text-3xl font-medium">Products</h1>
-        <p className="font-medium text-lg select-none text-neutral-400 hover:underline cursor-pointer" onClick={() => {
-          this.setState({ productSlice: this.state.productSlice == 4 ? this.state.data?.length : 4 })
-        }}>{this.state.productSlice == 4 ? 'Show All' : "Show Less"}</p>
-        </div>
+          
+            <h1 className="text-3xl font-medium">Products</h1>
+          
           <div className="grid grid-cols-4 gap-6 ">
-            {this.state.data?.slice(0, this.state.productSlice).map((item: ProductsData) => (
-              <a href={`/about/${item.id}`} onClick={() => window.localStorage.setItem('productId', JSON.stringify(item.id))} className="col-span-1 hover:shadow-lg duration-300 flex flex-col h-[400px] border " key={item.id}>
-                <div className="relative py-10">
-                  <img
-                    src={item.image}
-                    className="h-[200px] w-[200px] object-contain mx-auto"
-                    alt=""
-                  />
-                  <p
-                    className={`${
-                      categoriesColor[item.category]
-                    } absolute bottom-3 left-3`}
-                  >
-                    {item.category}
-                  </p>
-                </div>
-                <div className="p-3 flex h-full justify-between flex-col">
-                  <div>
-                    <h1 className="line-clamp-1 hover:underline ">
-                      {item.title}
-                    </h1>
-                    <div className="flex items-center">
-                      <FaStar className="text-yellow-400" />
-                      <p className="text-neutral-500">{item.rating.rate}</p>
+            {this.state.data
+              ?.slice(0, this.state.productSlice)
+              .map((item: ProductsData) => (
+                <a
+                  href={`/about/${item.id}`}
+                  onClick={() =>
+                    window.localStorage.setItem(
+                      "productId",
+                      JSON.stringify(item.id)
+                    )
+                  }
+                  className="col-span-1 hover:shadow-lg duration-300 flex flex-col h-[400px] border "
+                  key={item.id}
+                >
+                  <div className="relative py-10">
+                    <img
+                      src={item.image}
+                      className="h-[200px] w-[200px] object-contain mx-auto"
+                      alt=""
+                    />
+                    <p
+                      className={`${
+                        categoriesColor[item.category]
+                      } absolute bottom-3 left-3`}
+                    >
+                      {item.category}
+                    </p>
+                  </div>
+                  <div className="p-3 flex h-full justify-between flex-col">
+                    <div>
+                      <h1 className="line-clamp-1 hover:underline ">
+                        {item.title}
+                      </h1>
+                      <div className="flex items-center gap-2">
+                        <FaStar className="text-yellow-400" />
+                        <p className="text-neutral-500">{item.rating.rate}</p>
+                      </div>
+                    </div>
+                    <div className="flex items-center justify-between">
+                      <p className="font-medium text-2xl text-neutral-600">
+                        ${item.price.toFixed()}
+                      </p>
+                      <span className="p-3 border border-transparent hover:border-neutral-400 hover:shadow-xl duration-300 rounded-full">
+                      <CgEye className="scale-125"/>
+                      </span>
                     </div>
                   </div>
-                  <p className="font-medium ">${item.price.toFixed()}</p>
-                </div>
-              </a>
-            ))}
+                </a>
+              ))}
           </div>
+          <button
+              className="font-medium text-lg select-none w-full bg-neutral-300 py-3 rounded-md mt-5 hover:bg-neutral-400/80 cursor-pointer duration-300"
+                    onClick={() => {
+                      if (this.state.data) {
+                        this.setState({
+                          productSlice:
+                            this.state.productSlice < this.state.data.length ? this.state.productSlice + 6 : 4,
+                        });
+                      }
+                    }}
+            >
+              {this.state.data && this.state.productSlice <= this.state.data.length ? (
+                "Show More (6)"
+              ) : (
+                "Show Less (4)"
+              )}
+            </button>
         </div>
       </section>
     );
